e7b15a9454b8a300d1cd24f6c23c30401b6afa78
Jerome Aquilina <jeromeaq@hotmail.co.uk>
2018-01-31 14:46:24 +0000

    creating a geometric pattern

--- Geometric patterns/Main.js
+++ Geometric patterns/Main.js
@@ -1,18 +1,28 @@
 var num_circles = 12;
 var circleDiam;
+var circleRad =circleDiam/2;
 
 function setup(){
     createCanvas(480,600); 
     circleDiam = width/num_circles;
+    circleRad =circleDiam/2;
 }
 function draw(){
+    var isShifted = false;
+    
     var y=0;
     while (y<=height){
+        
         var x=0;
+        if(isShifted){ x=circleRad;}
+        else{x=0;}
+        
         while(x<=width){
-            x= x+circleDiam;
             ellipse(x,y,circleDiam,circleDiam);
+            x= x+circleDiam;
         }
-        y= y +circleDiam;
+        
+        y= y +circleRad;
+        isShifted = !isShifted;
     }
 }
